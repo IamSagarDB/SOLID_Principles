2579d31fb076162fe65e02801b92710482e62091
sagar <sagardb50@gmail.com>
2023-03-11 22:46:52 +0530

    added Abstract Factory CP

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,4 @@
-import javax.xml.crypto.Data;
-import java.text.NumberFormat;
-import java.util.Calendar;
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 public class Main {
     public static void main(String[] args) {
-
-        String a = "ABCABC";
-        String b = "ABC";
-        System.out.println(isCheck(a,b));
-    }
-
-    private static  String isCheck(String a , String b){
-        if (a.length() == b.length()){
-            if (a.equals(b)){
-                return a;
-            }else {
-                return "";
-            }
-        }
-
-        if (a.length() < b.length()) {
-            String temp = a;
-            a = b;
-            b = temp;
-        }
-
-        if (!a.startsWith(b)) return "";
-
-        return isCheck(a.substring(b.length()), b);
     }
 }
-
-
